a0857f1b53487ad51ad0f6ab9bcfa46c5b2f09f2
sergey080888 <237264@mail.ru>
2022-06-26 10:21:29 +0300

    первый commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from pprint import pprint
+import requests
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
+        href = self._get_upload_link(file_path=file_path).get("href", "")
+        response = requests.put(href, data=open(path_to_file, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = "1.txt"
+    token = ''
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
